50de4b3e3be9da4e1cf64c36e98627bb6543f821
Benoit Mathiez <benoit.mathiez@saintmichelannecy.fr>
2023-04-29 18:35:26 +0200

    Ajoutez des fichiers projet.

---
+++ SacJen.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>SacEnCuir</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>	
+	
+	
+	<div class="container">
+		<div class="row">
+			
+			<div class="col">
+				<h1>SacEnCuir</h1>
+
+				<?php include("dataPHP.php"); ?>
+			</div>
+			
+		</div>
+	</div>
+	
+
+
+
+	
+
+</body>
+</html>
